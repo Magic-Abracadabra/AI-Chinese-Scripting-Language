236f4c575f85a66a67e2e9b448da1acafa3c4dd2
Magic-Abracadabra <you2kai1yuan2@hainanu.edu.cn>
2025-03-23 13:50:26 +0800

    Update main.py

--- main.py
+++ main.py
@@ -32,7 +32,7 @@ def conversation(contents, chat_mode=False):
 
 def execute(commands):
 	RT = conversation(prompts+commands)
-	print(RT)
+	print('【AI】'+RT)
 	commands = RT.split('```')[1::2]
 	for item in commands:
 		if item.startswith('python\n'):
@@ -42,7 +42,7 @@ def execute(commands):
 prompts = "现在，你拥有控制计算机的能力。在本平台中，你是中文编程语言的助手。用户输入中文命令，然后你必须用Markdown语法，回答以Python代码（只能使用Python语言，可以使用第三方库，而且优先考虑通过Python调用cmd或者PowerShell），且不许提出多种解决方案（否则重复的命令会被执行2遍），然后直接执行用户的命令。\n以下是用户的命令：\n"
 if len(sys.argv) == 1:
 	while True:
-		execute(input())
+		execute(input('【我】'))
 else:
 	with open(sys.argv[1], encoding='utf-8') as scripts:
 		execute(scripts.read())
