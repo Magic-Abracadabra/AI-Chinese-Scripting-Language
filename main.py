dae018c1a66e4e348ea1939ef4a04ac4f06a67b8
Magic-Abracadabra <you2kai1yuan2@hainanu.edu.cn>
2025-03-24 14:26:52 +0800

    Update main.py

--- main.py
+++ main.py
@@ -38,7 +38,8 @@ def execute(commands):
 		if item.startswith('python\n'):
 			exec(item[7:])
 
-prompts = "现在，你拥有控制计算机的能力。在本平台中，你是中文编程语言的助手。用户输入中文命令，然后你必须用Markdown语法，回答以Python代码（只能使用Python语言，可以使用第三方库，而且优先考虑通过Python调用cmd或者PowerShell），且不许提出多种解决方案（否则重复的命令会被执行2遍），然后直接执行用户的命令。\n以下是用户的命令：\n"
+prompts = "现在，你拥有控制计算机的能力，你可以控制鼠标键盘，并且可以读取剪切板。在本平台中，你是中文编程语言的助手。用户输入中文命令，然后你必须用Markdown语法，回答以Python代码（只能使用Python语言，可以使用第三方库，而且优先考虑通过Python调用cmd或者PowerShell），且不许提出多种解决方案（否则重复的命令会被执行2遍），然后直接执行用户的命令。\n以下是用户的命令：\n"
+history = ''
 if len(sys.argv) == 1:
 	while True:
 		user_input = input('【我】')
@@ -50,6 +51,8 @@ if len(sys.argv) == 1:
 			except:
 				print('【出错，重新尝试】')
 				user_input+='\n实际执行过程中出错，报错信息如下：'+traceback.format_exc()+'\n请你针对错误，重新给出修正版的完整解决方案，不要输出之前的代码，直接输出你更正之后的代码'
+		history += user_input
+		history += '\n'
 else:
 	with open(sys.argv[1], encoding='utf-8') as scripts:
 		execute(scripts.read())
